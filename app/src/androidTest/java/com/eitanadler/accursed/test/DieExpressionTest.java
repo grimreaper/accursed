62d130cc04534f2cfd4c2f1fc0d97d76e3c605ce
Eitan Adler <lists@eitanadler.com>
2014-07-20 23:38:58 -0700

    junit3 is the way to go for android :'(
    actually do some validation of DieExpression
    
    switch to java 1.7 -- because I can

--- app/src/androidTest/java/com/eitanadler/accursed/test/DieExpressionTest.java
+++ app/src/androidTest/java/com/eitanadler/accursed/test/DieExpressionTest.java
@@ -4,8 +4,6 @@ import android.test.AndroidTestCase;
 
 import com.eitanadler.accursed.dice.DieExpression;
 
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
@@ -13,18 +11,20 @@ import org.robolectric.RobolectricTestRunner;
  * Created by eax on 7/20/14.
  */
 @RunWith(RobolectricTestRunner.class)
-public class DieExpressionTest extends AndroidTestCase {
+public class DieExpressionTest extends AndroidTestCase{
 
-    public ExpectedException exception = ExpectedException.none();
-
-    @Test
     public void test_expression() {
         DieExpression de = new DieExpression("1d6");
         assertEquals(de.getExpression(), "1d6");
     }
 
     public void test_reject_invalid_expression() {
-        exception.expect(DieExpression.InvalidDieExpression.class);
-        DieExpression de = new DieExpression("1d");
+        try {
+            DieExpression de = new DieExpression("1d");
+            fail("InvalidDieExpression not thrown");
+        }
+        catch (DieExpression.InvalidDieExpression exception) {
+            // swallow
+        }
     }
 }
--- app/src/main/java/com/eitanadler/accursed/dice/DieExpression.java
+++ app/src/main/java/com/eitanadler/accursed/dice/DieExpression.java
@@ -2,13 +2,16 @@ package com.eitanadler.accursed.dice;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Created by eax on 7/20/14.
  */
 public class DieExpression {
 
-    public class InvalidDieExpression extends IllegalArgumentException {};
+    public static class InvalidDieExpression extends IllegalArgumentException {};
 
     @NotNull
     protected String expression;
@@ -26,7 +29,41 @@ public class DieExpression {
     }
 
     public DieExpression(@NotNull String expression) {
+        DieExpression.validateDieExpression(expression);
         this.expression = expression;
+
     }
+
+    static protected void validateDieExpression(@NotNull String expr) {
+        String[] dice_split = expr.split("\\+");
+        for (String die : dice_split) {
+            String[] die_specifics = expr.split("d");
+            try {
+                Integer amt = Integer.parseInt(die_specifics[0]);
+                Integer val = Integer.parseInt(die_specifics[1]);
+            }
+            catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+                throw new DieExpression.InvalidDieExpression();
+            }
+        }
+    }
+
+    static public List<UnweightedDie> DieExpressionToDieList(DieExpression de) {
+        //TODO: implement error handling
+        List<UnweightedDie> ls = new ArrayList<UnweightedDie>();
+        String expr = de.getExpression();
+        String[] dice_split = expr.split("\\+");
+        for (String die : dice_split) {
+            String[] die_specifics = expr.split("d");
+            Integer amt = Integer.parseInt(die_specifics[0]);
+            Integer val = Integer.parseInt(die_specifics[1]);
+            UnweightedDie ud = new UnweightedDie(val);
+            while (amt-- > 0) {
+                ls.add(ud);
+            }
+        }
+        return ls;
+    }
+
 
 }
