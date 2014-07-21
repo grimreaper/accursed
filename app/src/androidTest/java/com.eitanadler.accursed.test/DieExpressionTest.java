d4b648d3586f31895165b0934216ccc73fa2af68
Eitan Adler <lists@eitanadler.com>
2014-07-20 21:19:45 -0700

    Attempt to get junit working -- again

---
+++ app/src/androidTest/java/com.eitanadler.accursed.test/DieExpressionTest.java
@@ -0,0 +1,22 @@
+package com.eitanadler.accursed.test;
+
+import android.test.InstrumentationTestCase;
+
+import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.TestCase.assertEquals;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * Created by eax on 7/20/14.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class DieExpressionTest extends InstrumentationTestCase {
+
+    @Test
+    public void test_something() {
+        assertEquals(0,1);
+    }
+}
